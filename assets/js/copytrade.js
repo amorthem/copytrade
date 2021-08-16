b9058bff211d97307cea6727885efe5760e94295
amorthem <noom-pn@hotmail.com>
2021-08-17 02:08:33 +0700

    first commit

---
+++ assets/js/copytrade.js
@@ -0,0 +1,150 @@
+/*
+* On page load
+*/
+window.addEventListener("load", () => {
+    new TradingView.widget(
+        {
+        "width": "99%",
+        "height": 615,
+        "symbol": "BINANCE:BTCUSDT",
+        "interval": "60",
+        "timezone": "Etc/UTC",
+        "theme": "dark",
+        "style": "1",
+        "locale": "th_TH",
+        "toolbar_bg": "#f1f3f6",
+        "enable_publishing": false,
+        "hide_side_toolbar": false,
+        "allow_symbol_change": true,
+        "container_id": "tradingview_8c3f4"
+      }
+        );
+});
+
+let logsTrading = document.querySelector("#logsTrading");
+let logsHistory = document.querySelector("#logsHistory");
+
+/*
+ * @function Cut USDT in token pair
+ * Example.
+ *   - BNBUSDT => BNB
+ *   - DOGEUSD => DOGE
+ */
+const symbolPair1 = (token) => {
+    return token.substr(0, token.length - 4); // -4 is USDT (Pair2)
+  };
+
+
+/*
+* @function push Assets table
+*/
+const addAssetsWatchList = (symbol) => {
+document.querySelector("#tokenCard").innerHTML += `
+    <li class="list-group-item bg-dark text-light" onclick="reloadComponents('${symbol}')">
+        <div class="d-flex justify-content-between">
+          <strong><span id="symbol${symbol}"></span></strong>
+          <div>
+            <span class="text-light" id="price${symbol}"></span>
+            <small id="priceColor${symbol}">( <i class="fas fa-caret-down text-light" id="priceUpDown${symbol}"></i> <span id="percentChange${symbol}"></span>)</small>
+          </div>
+        </div>
+    </li>
+    `;
+};
+
+/*
+* @function Load Assets IMG
+*/
+
+const loadAssetsImg = async (symbols) => {
+  const response = await fetch("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&symbols=" +symbolPair1(symbols).toLowerCase());
+  const tokenData = await response.json();
+
+  const response2 = await fetch("https://tokens.venus.io/");
+  const tokenData2 = await response2.json();
+  
+   
+  document.querySelector("#symbol" + symbols).innerHTML = `<img src="${tokenData[0].image}" height="30" class="rounded-circle"> &nbsp;&nbsp;${symbolPair1(symbols)}`;
+};
+/*
+* @function Load Assets
+*/
+const getAssets = (symbol) => {
+  addAssetsWatchList(symbol);
+  loadAssetsImg(symbol);
+  document.querySelector("#assetLists").innerHTML += `<option value="${symbol}">${symbol}</option>`;
+
+  setInterval(() => {
+    let txtPrice = document.querySelector("#price" + symbol);
+    let priceColor = document.querySelector("#priceColor" + symbol);
+    let priceUpDown = document.querySelector("#priceUpDown" + symbol);
+    let percentChange = document.querySelector("#percentChange" + symbol);
+
+    fetch("https://api.binance.com/api/v3/ticker/24hr?symbol=" + symbol)
+      .then(async (response) => {
+        return response.json();
+      })
+      .then((data) => {
+        //console.log(data.price);
+        let prices = data.lastPrice;
+        let price = parseFloat(prices).toFixed(4);
+        let percentChange24 = data.priceChangePercent;
+        let p0 = txtPrice.innerHTML.toLocaleString();
+        let p1 = price.toLocaleString();
+
+        if (p1 >= p0) {
+          priceColor.setAttribute("class", "text-success");
+          priceUpDown.setAttribute("class", "fas fa-caret-up");
+          txtPrice.innerHTML = price;
+          percentChange.innerHTML = percentChange24 + " %";
+        } else {
+          priceColor.setAttribute("class", "text-danger");
+          priceUpDown.setAttribute("class", "fas fa-caret-down");
+          txtPrice.innerHTML = price;
+          percentChange.innerHTML = percentChange24 + " %";
+        }
+      });
+  }, 500);
+};
+
+
+/*
+ * @function reload components
+ *   - News
+ *   - Chart
+ *   - Assets to Invest
+ */
+const reloadComponents = (symbol) => {
+  document.querySelector("#assetLists").value = symbol;
+  document.querySelector("#news-articles").setAttribute("src", `https://lunarcrush.com/widgets/news?symbol=${symbolPair1(symbol)}&interval=1 Week&animation=false&theme=dark`);
+
+  new TradingView.widget({
+    width: "99%",
+    height: 620,
+    symbol: "BINANCE:" + symbol,
+    interval: "60",
+    timezone: "Etc/UTC",
+    theme: "dark",
+    style: "1",
+    locale: "th_TH",
+    toolbar_bg: "#f1f3f6",
+    enable_publishing: false,
+    hide_side_toolbar: false,
+    allow_symbol_change: true,
+    container_id: "tradingview_8c3f4",
+  });
+};
+
+
+const _logsTrading = () => {
+    // document.querySelector("btnLogsTrading").setAttribute("class", "btn btn-secondary btn-md");
+    // document.querySelector("btnLogsHistory").setAttribute("class", "btn btn-light btn-sm");
+    logsTrading.setAttribute("style", "display: block-inline;");
+    logsHistory.setAttribute("style", "display: none;");
+}
+const _logsHistory = () => {
+    //document.querySelector("btnLogsHistory").setAttribute("class", "btn btn-light btn-sm");
+    //document.querySelector("btnLogsHistory").setAttribute("class", "btn btn-secondary btn-md");
+    logsTrading.setAttribute("style", "display: none;");
+    logsHistory.setAttribute("style", "display: block-inline;");
+}
--- assets/js/web3Provider.js
+++ assets/js/web3Provider.js
@@ -1,6 +1,7 @@
 
- window.addEventListener('load', function() {
+ window.addEventListener('load', async () => {
     // Check if Web3 has been injected by the browser (Mist/MetaMask).
+
     if (typeof window.ethereum === 'undefined') {
       // Use Mist/MetaMask's provider.
       alert('Please install Metamask. from metamask.io');
@@ -18,8 +19,8 @@ let WalletAccounts = "0x0000000000000000000000000000000000000000"; // set defaul
 const CACaddress = "0x369BF6FC83FdAB81AbB2AB41D0CB7206b3A5c584";
 const CoinFlipAddress = "0x40d6765DD003202E11E70fe86947d51DD269Cb77";
 // Set Varialble after Get Account Address
-const CACtoken = new web3.eth.Contract(CACabi, CACaddress);
-const ConFlip = new web3.eth.Contract(CoinFlipABI, CoinFlipAddress);
+//const CACtoken = new web3.eth.Contract(CACabi, CACaddress);
+//const ConFlip = new web3.eth.Contract(CoinFlipABI, CoinFlipAddress);
 let ApproveFromCAC = 0;
 let CACbalance = 0;
 let CACreward = 0;
@@ -37,20 +38,15 @@ const web3getAccount = async () => {
     const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
     // const accounts = await web3.eth.getAccounts(); 
     WalletAccounts = accounts[0];
-    document.querySelector("#wlletAccount").innerHTML = `<button class="btn btn-dark dtn-sm text-warning">${WalletAccounts.substr(0,4)}...${WalletAccounts.substr(-4)}</button>`;
-    checkCACBalance(WalletAccounts);
-    playerReward(WalletAccounts);
-    playerApproveFromCAC(WalletAccounts, CoinFlipAddress);
+    console.log(`Metamask Account : %c${WalletAccounts.substr(0, 6)}...${WalletAccounts.substr(-4)}`, "color: #ff0000");
+    document.querySelector("#metamaskAccount").innerHTML = `${WalletAccounts.substr(0, 6)}...${WalletAccounts.substr(-4)}`;
 
 
     // Time to reload your interface with accounts[0]!
     ethereum.on('accountsChanged', function (accounts) {
       WalletAccounts = accounts[0];
-      document.querySelector("#wlletAccount").innerHTML = `<button class="btn btn-dark dtn-sm text-warning">${WalletAccounts.substr(0,4)}...${WalletAccounts.substr(-4)}</button>`;
-      checkCACBalance(WalletAccounts);
-      playerReward(WalletAccounts);
-      playerApproveFromCAC(WalletAccounts, CoinFlipAddress);
-
+      console.log(`%c ${WalletAccounts.substr(0, 6)}...${WalletAccounts.substr(-4)}`, "color: #ff0000");
+      document.querySelector("#metamaskAccount").innerHTML = `${WalletAccounts.substr(0, 6)}...${WalletAccounts.substr(-4)}`;
     });
 
     
@@ -100,6 +96,7 @@ const playerApproveFromCAC = async (WalletAccountss, CoinFlipAddresss) => {
       }
     });
 }
+
 // Add CAC Allowance Function
  const addAllowance = async () => {
   let amountMaxWei = web3.utils.toWei("1000000000000");
@@ -115,94 +112,6 @@ const playerApproveFromCAC = async (WalletAccountss, CoinFlipAddresss) => {
 
 }
 
-
-// Betting Function
-const playerBet = async (coinSide) => {
-
-  let inputMoney = document.querySelector("#inputMoney");
-  let betAmount = web3.utils.toWei(inputMoney.value);
-  let _coinSide;
-
-  if(Number(inputMoney.value) <= 0 || Number(inputMoney.value) > Number(CACbalance))
-  {
-    console.log(Number(inputMoney.value));
-    console.log(Number(CACbalance));
-    checkINputAndBalance();
-    return;
-  }
-
-  //console.log(betAmount);
-  if(coinSide == 'heads')
-  {
-    _coinSide = 0;
-  } else if(coinSide == 'tails'){
-    _coinSide = 1;
-  }
-  await ConFlip.methods.betting(_coinSide, betAmount).send({ from: WalletAccounts, gas: 400000}, function(error, transactionHash){
-    if(error){
-      console.log(error);
-      return;
-    }
-      console.log(transactionHash);
-      $("#btn-heads").hide();
-      $("#btn-tails").hide();
-      $("#gameControl").hide();
-      $("#waitBetting").show();
-    }
-  )
-  .then((res) => {
-    betResult();
-    flip(coinSide);
-  });
-}
-
-// Get Bet Result Function
-const betResult = async () => {
-  await ConFlip.methods
-  .gameResult(WalletAccounts)
-  .call((err, res) => {
-    if (err) {
-      console.log("An error occured", err)
-      return
-    }
-      _gameResult = res;
-    });
-}
-
-// Get CAC Reward Function
-const playerReward = async () => {
-  await ConFlip.methods
-  .playerReward(WalletAccounts)
-  .call((err, res) => {
-    if (err) {
-      console.log("An error occured", err)
-      return
-    }
-      let balanceOfplayerReward = web3.utils.fromWei(res);
-      CACreward = balanceOfplayerReward;
-      // console.log(CACreward);
-      document.querySelector("#div-reward").innerHTML = parseFloat(CACreward).toFixed(2);
-    });
-}
-
-// Add CAC Allowance Function
-const withdrawReward = async () => {
-  let amount = web3.utils.toWei(CACreward);
-
-  await ConFlip.methods.withdrawReward(amount).send({ from: WalletAccounts, gasLimit: 300000}, (err, res) => {
-    if (err) {
-      console.log("An error occured", err)
-      return
-    }
-     console.log(res);
-     
-    })
-    .then(() => {
-      playerReward();
-    });
-
-}
-
 const onClickConnect = async () => {
   try {
     // Will open the MetaMask UI
@@ -213,24 +122,6 @@ const onClickConnect = async () => {
   }
 };
 
-/*============================================================
-====================== Write Contract ======================*/
-// Get CAC Reward Function
-const callBetting = async (WalletAccounts) => {
-  await ConFlip.methods
-  .playerReward(WalletAccounts)
-  .call((err, res) => {
-    if (err) {
-      console.log("An error occured", err)
-      return
-    }
-      let balanceOfplayerReward = web3.utils.fromWei(res);
-      CACreward = balanceOfplayerReward;
-      console.log(CACreward);
-      document.querySelector("#div-balance").innerHTML = parseFloat(CACreward).toFixed(2);
-    });
-}
+//onClickConnect();
 
-// =============================================== //
 // Call Function
-playerReward();
